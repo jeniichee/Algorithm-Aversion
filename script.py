7face34f35221044bc2a2c3107399399a0985204
jeniichee <123319961+jeniichee@users.noreply.github.com>
2024-02-09 00:50:36 -0500

    updates

--- script.py
+++ script.py
@@ -1,7 +1,58 @@
 import alfred3 as al
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from scipy.stats import pearsonr
 
 exp = al.Experiment()
 exp += al.ForwardOnlySection(name="main") 
+
+# model 
+def pred(file, target):
+
+    # load file 
+    df = pd.read_csv("databases/"+file)
+    
+    # encode categorical features
+    df = pd.get_dummies(df) 
+
+    # split and train
+    X = df.drop(target, axis=1) # input features
+    y = df[target] # target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+    
+    # # StandardScaler to normalize numeric data 
+    # scaler = StandardScaler() 
+    # scaler.fit(X_train)
+    # X_train = scaler.transform(X_train) # transform training data
+    # X_test = scaler.transform(X_test) # transform test data
+
+    # the model
+    rf = RandomForestRegressor(n_estimators=100, random_state=7)
+    rf.fit(X_train, y_train) 
+
+    # predict labels for test
+    y_pred = rf.predict(X_test)
+    
+    # converting y_pred 
+    predictions = pd.DataFrame()
+    predictions['Predictions'] = y_pred.tolist()
+    df_out = pd.merge(X_test, predictions, left_index = True, right_index = True)
+        
+    predictions['True Classes'] = y_test
+    predictions['Error'] =  predictions['True Classes'] - predictions['Predictions'] 
+    df_out = pd.merge(X_test, predictions, left_index = True, right_index = True)
+    print(df_out.head(100))
+    
+    # accuracy
+    print('Accuracy: {}'.format((rf.score(X_test, y_test))*100))
+    
+    # calculate the Pearson correlation coefficient
+    correlation_coefficient, _ = pearsonr(y_pred, y_test)
+    print(f"r: {correlation_coefficient}")
 
 # setup 
 @exp.setup
@@ -24,7 +75,7 @@ class Page1(al.Page):
     title = "File"
 
     def on_exp_access(self):
-        self += al.FileUpload(name="file_upload", label="Choose CSV File")
+        self += al.TextEntry(toplab="Please enter the file name:", name="file",  align="center")
         self += al.TextEntry(toplab="Please enter the target feature:", name="target",  align="center")
 
 # task 
@@ -33,9 +84,9 @@ class Page2(al.Page):
     title = "Task"
 
     def on_exp_access(self):
-        file_path = self.exp.values["databases/"+"file_upload"]
-        target = self.exp.values["target_entry"]
-        model.pred(file_path, target)
+        file_path = 
+        target = 
+        pred(file_path, target)
         
         self += al.TextEntry(toplab="Please enter your prediction:", name="prediction",  align="center")
            
