80e3a5e5572d4806d23e6f2b081a1ccd1613e25a
jeniichee <123319961+jeniichee@users.noreply.github.com>
2024-02-14 23:00:47 -0600

    updates

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import alfred3 as al
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
+from alfred3.admin import SpectatorPage
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr
+
 
 # model
 def pred(file, target):
@@ -65,25 +68,26 @@ exp.main.page0 += al.Text("*Instructions*", align="center")
 
 ## turn into admin page/behind the scenes 
 exp.main += al.Page(name="page1")
-exp.main.page1 += al.TextEntry(toplab="Please enter the file name:", name="t1")
+exp.main.page1 += al.TextEntry(toplab="Please enter the file name:", name="uploaded_file")
 exp.main.page1 += al.TextEntry(toplab="Please enter the target feature:", name="t2")
 
 @exp.member(of_section="main")
 class Page2(al.Page):
 
     def on_first_show(self):
-        input_on_page1 = self.exp.values["t1"]
-        input_on_page2 = self.exp.values["t2"]
-        preds, accuracy, correlation_coefficient = pred(input_on_page1, input_on_page2)
+        uploaded_file = self.exp.values["uploaded_file"]
+        target_feature = self.exp.values["t2"]
+        preds, accuracy, correlation_coefficient = pred(uploaded_file, target_feature)
         
-        # df = preds.to_string(index=False)
-        df = preds
+        self += al.Html(html=preds.to_html())
         
-        # self += al.MatPlot
         self += al.Text(f"Accuracy: {accuracy:.2f}%")
         self += al.Text(f"Pearson correlation coefficient: {correlation_coefficient:.2f}")
          
         self += al.TextEntry(toplab="Please enter your prediction:", name="prediction", align="center")
+
+
+        
 
 if __name__ == "__main__":
     exp.run()
@@ -115,3 +119,4 @@ if __name__ == "__main__":
 # star type = 0.9999321798727429
 # tip = 0.7569161669495375
 
+# go from admin page -> normal page 
