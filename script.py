347497f864f8627f14de8f1e0f00f19edd429b2a
jeniichee <123319961+jeniichee@users.noreply.github.com>
2024-02-29 19:09:28 -0500

    updates

--- script.py
+++ script.py
@@ -1,16 +1,20 @@
-# http://127.0.0.1:5000/start  
+"""
+http://127.0.0.1:5000/start  
 
+"""
 import pandas as pd
 import alfred3 as al
+import os
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr
 
+# prediction function 
 def pred(file, target):
 
     # load file 
-    df = pd.read_csv("databases/"+file)
+    df = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), "databases/", file))
     
     # optional drop columns 
     # uploaded_file = uploaded_file.drop()
@@ -39,43 +43,49 @@ def pred(file, target):
 
     # converting y_pred 
     predictions = pd.DataFrame()
-    predictions['predictions'] = y_pred.tolist()
-    # predictions['True Classes'] = y_test
+    predictions['predictions'] =[round(val) for val in y_pred.tolist()]
+    predictions['true value'] = round(y_test)
     # predictions['Error'] =  predictions['True Classes'] - predictions['Predictions'] 
 
     df_out = df.merge(X_test, left_index=True, right_index=True, how='inner')
-    df_out = df_out.drop(columns=['total_bill_y', 'size_y', 'sex_Female', 'sex_Male', 'smoker_No', 'smoker_Yes', 'day_Fri', 'day_Sat', 'day_Sun', 'day_Thur', 'time_Dinner', 'time_Lunch', 'tip'], axis=1)
+    df_out = df_out.drop(columns=['total_bill_y', 'size_y', 'sex_Female', 
+                                  'sex_Male', 'smoker_No', 'smoker_Yes', 
+                                  'day_Fri', 'day_Sat', 'day_Sun', 'day_Thur', 
+                                  'time_Dinner', 'time_Lunch', 'tip'], axis=1) # drop encoded columns
 
-    df_out = df_out.merge(predictions, left_index = True, right_index = True)
+    df_out_preds = df_out.merge(predictions, left_index = True, right_index = True)
     
-    # accuracy
-    accuracy = rf.score(X_test, y_test)*100
+    # rename columns 
+    df_out.rename(columns = {"total_bill_x": "total bill", "size_x": "number of guests"}, inplace = True)
+    df_out_preds.rename(columns = {"total_bill_x": "total bill", "size_x": "number of guests"}, inplace = True)
     
-    # calculate the Pearson correlation coefficient
-    correlation_coefficient, _ = pearsonr(y_pred, y_test)
+    # # accuracy
+    # accuracy = rf.score(X_test, y_test)*100
     
-    return df_out.head(100), accuracy, correlation_coefficient
+    # # calculate the Pearson correlation coefficient
+    # correlation_coefficient, _ = pearsonr(y_pred, y_test)
     
+    return df_out.head(100), df_out_preds.head(100)
+
+# create experiment 
 exp = al.Experiment()
+trials = 10 
 
 # setup
+# TODO: 
 @exp.setup
 def setup(exp):
     exp.progress_bar = al.ProgressBar(show_text=True)
+    
+exp += al.HideOnForwardSection(name="setup_section")
+exp.setup_section += al.Page(name="setup_page")
+exp.setup_section.setup_page += al.SingleChoiceButtons('Human', 'Algorithm', 'Toad', name = 'condition')
 
-# TODO: start section 
-# exp += al.ForwardOnlySection(name="consent")
-# exp.consent += al.Page(name="pdf")
-
-# TODO: consent section 
-# exp += al.ForwardOnlySection(name="consent")
-# exp.consent += al.Page(name="pdf")
-
-# instructions section 
-exp += al.ForwardOnlySection(name="main")
-exp.main += al.Page(name="welcome_pg")
-exp.main.welcome_pg += al.Text("Welcome!", align="center")
-exp.main.welcome_pg += al.Text("""
+# Section 01: instructions section 
+exp += al.ForwardOnlySection(name="section01")
+exp.section01 += al.Page(name="welcome_pg")
+exp.section01.welcome_pg += al.Text("Welcome!", align="center")
+exp.section01.welcome_pg += al.Text("""
 We are researchers from Queen\'s University, Belfast. We are investigating judgment and decision making.
 In this study, you\'ll be asked to make judgments and predictions. The study should take __ minutes to complete. 
 Your participation is entirely voluntary. You can end the survey at any point, for any reason, without penalty. 
@@ -83,9 +93,47 @@ Your responses will be anonymised and the data will be held securely.
 This means there is no way for anybody to possibly link the data to you. 
 This includes us as the researchers, which means that once you complete the study, 
 we will not have the means to withdraw your data after this point.
+Why is this important? 
+Since the data we collect from you may be of interest to other researchers, 
+we will publish it on a publicly accessible online data repository such as the Open Science Framework 
+(https://www.osf.io), where it will remain indefinitely. 
+That means, upon publication of the data set, anyone will have access to your anonymised (i.e., non-identifiable) data.   
 """, align="center")
 
-@exp.member(of_section="main")
+# TODO: import consent page 
+exp.section01 += al.Page(title="consent", name="consent_pg")
+exp.section01.consent_pg += al.Text("Please select each box if you consent to each statement.")
+exp.section01.consent_pg += al.Text("YOU CANNOT PROCEED TO THE SURVEY WITHOUT RESPONDING TO EACH STATEMENT")
+exp.section01.consent_pg += al.Hline()
+exp.section01.consent_pg += al.VerticalSpace("10px")
+exp.section01.consent_pg += al.MultipleChoice("Yes", "No", toplab="I have read and understood the information about the study.", name="m1")
+
+# Section 02: feedback 
+# TODO: ask if he means trials like this? 
+exp += al.HideOnForwardSection(name="feedback_section")
+
+for i in range(1, trials+1):
+    exp.feedback_section += al.Page(name=f"trial{i}_page1")
+    exp.feedback_section += al.Page(name=f"trial{i}_page2") 
+    
+# Page 1: shows cues for this trial only w/out algorithm prediction yet + asks participants to enter initial estimate
+@exp.member(of_section="feedback_section")
+class Page1(al.Page):
+
+    def on_first_show(self):
+        
+        # file/target input 
+        uploaded_file = "tips.csv"
+        target_feature = "tip"
+        preds, no_preds = pred(uploaded_file, target_feature)
+
+        self += al.Html(html=preds.to_html(), name="table")
+        self += al.Hline()
+        exp.section01.consent_pg += al.VerticalSpace("10px")
+        self += al.TextEntry(toplab="Please enter your prediction:", name="prediction", align="center")
+        
+#  Page 2: shows the cues, the advisor’s estimate, and the true value.
+@exp.member(of_section="feedback_section")
 class Page2(al.Page):
 
     def on_first_show(self):
@@ -93,18 +141,20 @@ class Page2(al.Page):
         # file/target input 
         uploaded_file = "tips.csv"
         target_feature = "tip"
-        preds, accuracy, correlation_coefficient = pred(uploaded_file, target_feature)
+        preds, no_preds = pred(uploaded_file, target_feature)
 
-        html_element = al.Html(html=preds.to_html(), name="table", align="center")
-        self += html_element
+        self += al.Html(html=preds.to_html(), name="table")
         self += al.Hline()
-        self += al.Text(f"Accuracy: {accuracy:.2f}%")
-        self += al.Text(f"Pearson correlation coefficient: {correlation_coefficient:.2f}")
+        exp.section01.consent_pg += al.VerticalSpace("10px")
         self += al.TextEntry(toplab="Please enter your prediction:", name="prediction", align="center")
         
-# TODO: confidence levels   
-     
+# Section 03: 
+# TODO: confidence levels
+   
+
+# Section 04: 
 # TODO: final section of the experiment - feedback, payment, and debriefing
+
 
 if __name__ == "__main__":
     exp.run()
